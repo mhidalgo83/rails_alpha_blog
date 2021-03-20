d539d35e7d4320cff4be609c8fd3b258903ececf
Matthew Hidalgo <makeshifthidalgo@hotmail.com>
2021-03-19 17:01:52 -0700

    downcasing of emails implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :articles
   validates :username, uniqueness: {case_sensitive: false}, presence: true, length: {minimum: 3, maximum: 25}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
