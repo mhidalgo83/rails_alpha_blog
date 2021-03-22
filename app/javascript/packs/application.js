c823d3f69e47323556a1e06a66f8636173f23283
Matthew Hidalgo <makeshifthidalgo@hotmail.com>
2021-03-22 10:07:32 -0700

    sticky footer and styling implemented

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,6 @@
 // that code so it'll be compiled.
 
 import "bootstrap/dist/js/bootstrap";
-import "bootstrap/dist/css/bootstrap";
 import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
