4625f8ebb1970e60d4b989de6912406d05a4d323
Matthew Hidalgo <makeshifthidalgo@hotmail.com>
2021-03-28 11:54:34 -0700

    users signed into application after registering

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:session][:email].downcase)
-    if @user && @user.authenticate(params[:session][:password])
-      session[:user_id] = @user.id
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
       flash[:notice] = "Logged in successfully"
-      redirect_to @user
+      redirect_to user
     else
       flash.now[:alert] = "There was something wrong with your log in details"
       render 'new'
